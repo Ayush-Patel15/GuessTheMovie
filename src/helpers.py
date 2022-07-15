3dd4c892a7d4960353db9ebe9fcd5e17dc044136
Ayush Patel <patelayushap1512@gmail.com>
2022-07-16 00:45:39 +0530

    fixes in helpers function

--- src/helpers.py
+++ src/helpers.py
@@ -1,14 +1,13 @@
-from get_movie import get_movie_name
-
-VOWELS = ["A","E","I","O","U"]
-INTEGERS = [0,1,2,3,4,5,6,7,8,9]
+# VOWELS = ["A","E","I","O","U"]
+ALPHABETS = "BCDFGHJKLMNPQRSTVWXYZ"
+VOWELS = "AEIOU"
+INTEGERS = "0123456789"
 SYMBOLS = ["-","!","$","&",",",".","'",":","?"]
 
-def movie_name_with_vowels():
-    movie = get_movie_name()
+def movie_name_with_vowels(movie):
     movie_name = ""
     for char in movie:
-        if (char in VOWELS) or (char in str(INTEGERS)) or (char in SYMBOLS):
+        if (char in VOWELS) or (char in INTEGERS) or (char in SYMBOLS):
             movie_name = movie_name + char + "  "
         else:
             movie_name = movie_name + "_" + "  "
@@ -17,4 +16,5 @@ def movie_name_with_vowels():
 
 if __name__ == "__main__":
     # "movie" should be in upper case letters.
-    print(movie_name_with_vowels())
+    # print(movie_name_with_vowels(get_movie_name()))
+    print(movie_name_with_vowels("BLACK PANTHER - 2"))
