32641b981510d42926561caffbb8d3ce3a7d24fe
Ayush Patel <patelayushap1512@gmail.com>
2022-07-18 16:47:37 +0530

    Added more helper functions

--- src/helpers.py
+++ src/helpers.py
@@ -1,10 +1,12 @@
+import random
+
 # VOWELS = ["A","E","I","O","U"]
 ALPHABETS = "BCDFGHJKLMNPQRSTVWXYZ"
 VOWELS = "AEIOU"
 INTEGERS = "0123456789"
 SYMBOLS = ["-","!","$","&",",",".","'",":","?"," "]
 
-def movie_name_guessing(movie, guessed=[]):
+def filling_movie_name(movie, guessed=[]):
     movie_name = ""
     for char in movie:
         if (char in VOWELS) or (char in INTEGERS) or (char in SYMBOLS) or (char in guessed):
@@ -14,7 +16,37 @@ def movie_name_guessing(movie, guessed=[]):
     # print(movie)
     return movie_name
 
+
+def number_of_players(min, max):
+    n = input("Number of Players: ")
+    try:
+        n = int(n)
+        if n <= min:
+            output = "There should be atleast 1 player to play the game."
+        elif n > max:
+            output = "Maximum limit of players is 4."
+        else:
+            output = n
+    except Exception:
+        output = "Invalid Integer! Please enter a valid integer."
+    # print(type(output))
+    return output
+
+
+def player_move(player):
+    guess = player.human_get_move()
+    return guess
+
+
+def wheel_spin():
+    option = random.choice(["CASH","LOSETURN","PRISON"])
+    return option
+
 if __name__ == "__main__":
     # "movie" should be in upper case letters.
-    # print(movie_name_guessing(get_movie_name()))
-    print(movie_name_guessing("BLACK PANTHER-2",["B","N","R"]))
+    # print(filling_movie_name(get_movie_name()))
+    print(filling_movie_name("BLACK PANTHER-2",["B","N","R"]))
+    # print(number_of_players(0, 4))
+    # obj = HumanPlayer("Ayush")
+    # print(player_move(obj))
+    # print(wheel_spin())
