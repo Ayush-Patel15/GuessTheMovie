a311db4a99cbc5e3caf86c1c8d2e6317ce96b164
Ayush Patel <patelayushap1512@gmail.com>
2022-07-20 15:20:47 +0530

    Added method to reward and punish the player for their correct and incorrect guesses

--- src/main.py
+++ src/main.py
@@ -9,11 +9,16 @@ index = 0
 guessed_letters = []
 winner = False
 
-print("############# HUMAN PLAYERS  #############")
+print("\n############# HUMAN PLAYERS  #############")
 num_of_human = number_of_players(0,4)
 
 if type(num_of_human) is int:
     human_players = [HumanPlayer(input("Enter Name of player {0}: ".format(i+1))) for i in range(num_of_human)]
+
+    print("\n")
+    print("=="*40)
+    print("\n")
+    [print("WELCOME {0}..! Your Prize Money is Rs.{1}/-".format(each.name,each.prize_money)) for each in human_players]
 
     print(movie_showboard(movie=movie_name, guessed=guessed_letters))
 
@@ -42,21 +47,27 @@ if type(num_of_human) is int:
                 guess = guess
                 guessed_letters.append(guess)
                 count = movie_name.count(guess)
+
+                if count <= 0:
+                    player.prize_money = player.prize_money - 50
+                else:
+                    player.add_prize_money(count)
+
                 print("{0}'s guesses {1}".format(player.name, guess))
                 print("Movie has {0} {1}.".format(count, guess))
-                print(movie_showboard(movie=movie_name, guessed=guessed_letters))
 
                 movie_title = filling_movie_name(movie=movie_name,guessed=guessed_letters)
                 if removing_spaces(movie_title) == removing_spaces(movie_name):
                     winner = player
                     break
 
-            else:
-                print("Invalid Guess")
+            print("{0}'s total prize money is Rs.{1}/-".format(player.name,player.prize_money))
+            print(movie_showboard(movie=movie_name, guessed=guessed_letters))
         
         else:
             if removing_spaces(guess) == removing_spaces(movie_name):
                 winner = player
+                player.add_prize_money(count=len(removing_spaces(guess)))
                 break
             else:
                 print("{0} is not the correct movie.".format(guess))
@@ -67,7 +78,7 @@ if type(num_of_human) is int:
     ##### WINNER DECLARATION
 
     if winner:
-        print("{0} WINS..! Correct movie name was {1}.".format(winner.name, movie_name))
+        print("{0} WINS..! Correct movie name was {1}.\nCongratulations..! Your winning prize amount is Rs.{2}/-".format(winner.name, movie_name,winner.prize_money))
     else:
         print("NoBody Wins..! Movie name was {0}".format(movie_name))
 else:
